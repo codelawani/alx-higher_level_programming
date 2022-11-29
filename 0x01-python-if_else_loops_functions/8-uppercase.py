8d3cccc4efea9b627394d6330ff6ce8a475bff34
Nico <lawani.gk@gmail.com>
2022-11-29 18:56:43 +0100

    refactor code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) > 96 and ord(c) < 123:
-            c = ord(c) + 32
-        print("{:c}".format(c), end='')
+        if ord(c) > 96:
+            c = chr(ord(c) - 32)
+        print("{:s}".format(c), end='')
+    print('')
