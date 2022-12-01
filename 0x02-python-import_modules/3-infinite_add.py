e9934e369d0433a61eb4a094655f8bcae233ea90
Nico <lawani.gk@gmail.com>
2022-12-01 08:40:19 +0100

    add new file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print(0)
+    else:
+        sum = 0
+        for i in range(1, argc + 1):
+            sum += int(sys.argv[i])
+        print(sum)
