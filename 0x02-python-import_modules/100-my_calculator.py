7ecfc9f900fd189cdb2d0296aace80d657dd31a4
Nico <lawani.gk@gmail.com>
2022-12-01 13:08:43 +0100

    add new file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+    ac = len(sys.argv) - 1
+    av = sys.argv
+    if ac != 3:
+        print(f"Usage: {av[0]:s} <a> <operator> <b>")
+        exit(1)
+    op = av[2]
+    a = int(av[1])
+    b = int(av[3])
+    if op == '+':
+        print(f"{a:d} + {b:d} = {add(a,b):d}")
+    elif op == '-':
+        print(f"{a:d} - {b:d} = {sub(a,b):d}")
+    elif op == '*':
+        print(f"{a:d} * {b:d} = {mul(a,b):d}")
+    elif op == '/':
+        print(f"{a:d} / {b:d} = {div(a,b):d}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    exit(0)
