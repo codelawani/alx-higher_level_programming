8a99fc931a27692e01893aaa6ba4388556862d77
Nico <lawani.gk@gmail.com>
2022-12-04 06:58:35 +0100

    refactor code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(*row)
+        n = 0
+        for element in row:
+            if n > 0:
+                print(" ", end ="")
+            print("{:d}".format(element), end ="")
+            n = 1
+        print("")
