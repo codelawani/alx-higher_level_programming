6dae346d42a7d3ea92ed37a00ea449f3e374e0ea
Nico <lawani.gk@gmail.com>
2022-12-04 09:45:44 +0100

    add new files

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-#    if len(tuple_b) == 2:
-    if not tuple_b[1]:
-        b = 0
-    if not tuple_a[1]:
-        a = 0
-    x = tuple_a[0]
-    y = tuple_b[0]
-    a = tuple_a[1]
-    b = tuple_b[1]
-    print(f"b:{b}")
-    new = (x + y, a + b)
-    return new
+    la = len(tuple_a)
+    lb = len(tuple_b)
+    if la >= 2:
+        a0, a1 = tuple_a[0], tuple_a[1]
+    if lb >= 2:
+        b0, b1 = tuple_b[0], tuple_b[1]
+    if la == 1:
+        a0, a1 = tuple_a[0], 0
+    if lb == 1:
+        b0, b1 = tuple_b[0], 0
+    if la == 0:
+        a0, a1 = 0, 0
+    if lb == 0:
+        b0, b1 = 0, 0
+    new_t = (a0 + b0, a1 + b1)
+    return new_t
