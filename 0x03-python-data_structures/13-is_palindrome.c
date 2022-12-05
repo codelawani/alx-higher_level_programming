fcb17af0d4816f699f6956fbffa1e1c0bb752d94
Nico <lawani.gk@gmail.com>
2022-12-05 23:39:23 +0100

    modify files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * is_palindrome - checks if a list is palindrome
- * @head: pointer to address of the head of the list
- * Return: 1 if palindrome or 0 if not
+ * @head: pointer to address of head of node
+ * Return: 1 if palindrome or 0 otherwise
  */
 
 int is_palindrome(listint_t **head)
 {
-	int i = 0, j, count = 0;
-	listint_t *front, *rear, *current;
+	listint_t *current;
+	int *listnums, first = 0, last, listlen = 0;
 
 	if (*head == NULL)
 		return (1);
 	current = *head;
+	listnums = malloc(sizeof(int));
+	if (listnums == NULL)
+		return (0);
 	while (current != NULL)
 	{
-		count++;
+		listnums[listlen] = current->n;
 		current = current->next;
+		listlen++;
+		listnums = realloc(listnums, (1 + listlen) * sizeof(int));
 	}
-	while (i != count / 2)
+	last = listlen - 1;
+	while (listnums[first] == listnums[last])
 	{
-		front = rear = *head;
-		for (j = 0 ; j < i ; j++)
-			front = front->next;
-		for (j = 0 ; j < count - (i + 1) ; j++)
-			rear = rear->next;
-		if (front->n != rear->n)
-			return (0);
-		else
-			i++;
+		first++, last--;
+		if (first >= last)
+		{
+			free(listnums);
+			return (1);
+		}
 	}
-	return (1);
+	free(listnums);
+	return (0);
 }
