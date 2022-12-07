627a263fe5ed7efa50a0193051bc7dc91532ce73
Nico <lawani.gk@gmail.com>
2022-12-07 19:02:29 +0100

    refactor code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(dict(sorted(a_dictionary.items())))
+    new = dict(sorted(a_dictionary.items()))
+    keys = new.keys()
+    values = new.values()
+    for k, v in zip(keys, values):
+        print("{:s}: {}".format(k, v))
