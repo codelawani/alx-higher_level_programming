19cb9617fa4dd3c300636c8e1c13c348a0ad1318
Nico <lawani.gk@gmail.com>
2022-12-07 21:55:27 +0100

    refactor code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
-        return None
-    new_dict = dict(sorted(a_dictionary.items(), key = lambda item: item[1]))
-    last_key = list(new_dict.keys())[-1]
-    return last_key
+    return max(a_dictionary, key = a_dictionary.get) if a_dictionary else None
