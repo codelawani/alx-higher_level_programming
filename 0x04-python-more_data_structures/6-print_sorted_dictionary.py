2aea6e0c3a742a7357cd7433a10fa22db278ea39
Nico <lawani.gk@gmail.com>
2022-12-07 19:15:00 +0100

    add new files

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new = dict(sorted(a_dictionary.items()))
-    keys = new.keys()
-    values = new.values()
-    for k, v in zip(keys, values):
+    for k, v in zip(new.keys(), new.values()):
         print("{:s}: {}".format(k, v))
