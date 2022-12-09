9342fe3ba96d9c8382f671e09d59f0074b8b0147
lawani.gk@gmail.com <nico>
2022-12-09 16:11:02 +0100

    add new files

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    new_d = dict(my_list)
-    
+    if not my_list:
+        return
+    total_weight = 0
+    total_score = 0
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    return total_score / total_weight
