08e81ca990f4aefa8dbf7c27ca22f7086930f28e
Nico <lawani.gk@gmail.com>
2022-12-10 22:17:20 +0100

    refactor code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str:
         return 0
-    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    roman_dict = {'M': 1000, 'D': 500, 'C': 100,
+                  'L': 50, 'X': 10, 'V': 5, 'I': 1}
     nums = []
     for char in roman_string:
         for k, v in list(roman_dict.items()):
@@ -11,7 +12,7 @@ def roman_to_int(roman_string):
     number = 0
     num_of_nums = len(nums)
     for i in range(num_of_nums):
-        if nums[i] != nums[-1]:
+        if i != num_of_nums - 1:
             j = i + 1
             if nums[i] < nums[j]:
                 nums[i] = -nums[i]
