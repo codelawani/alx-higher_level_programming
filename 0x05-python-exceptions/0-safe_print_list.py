200474a2cd9fcaaa1a58faaa92c857bb0bc733c3
Nico <nico>
2022-12-19 12:24:09 +0100

    modify file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print(f'{my_list[i]}', end='')
-        except Exception:
+        except IndexError:
             print()
             return i
     print()
