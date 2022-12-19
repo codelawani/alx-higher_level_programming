d801d8a2f51ab7dccfa6a544f4bd8130abbce675
Nico <nico>
2022-12-19 16:23:37 +0100

    add new files with solutions to task 8. Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except Exception as error:
+        result = None
+        print(f'Exception: {error}',file=stderr)
+    finally:
+        return result
