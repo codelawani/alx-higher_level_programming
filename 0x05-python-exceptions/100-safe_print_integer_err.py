03e6b059dd6e71fe24ed8fdac4fa493778cca0aa
Nico <nico>
2022-12-19 16:11:56 +0100

    modify files

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         print(f'Exception: {err}', file=sys.stderr)
         return False
