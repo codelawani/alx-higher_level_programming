bc71ef7314c52d15f2f72010b0a764ce8b95b3b0
Nico <nico>
2022-12-19 16:33:03 +0100

    modify file

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,4 @@ def safe_function(fct, *args):
         return fct(*args)
     except Exception as error:
         print('Exception: {}'.format(error), file=sys.stderr)
-    finally:
         return None
