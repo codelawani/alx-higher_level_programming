0d860934dfedeb2d510046d6b83493f5bb2f0e49
Nico <nico>
2022-12-19 15:56:44 +0100

    add new files for task 7 - Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):    
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print(f'Exception: {err}', file=sys.stderr)
+        return False
