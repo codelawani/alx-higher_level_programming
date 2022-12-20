15f8e1a6ec59a5ac50f371fc3c1e5ac1e578f6ae
Nico <nico>
2022-12-20 12:47:26 +0100

    add new file for task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initialize square class
+        Args: size - size of defined square
+        """
+        self.__size = size
