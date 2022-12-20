fe42c7eeffacae126fd3cf085a07660a8600f90e
Nico <nico>
2022-12-20 13:14:13 +0100

    add new file for task 2. size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initialize square class
+        Args: size - size of defined square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
