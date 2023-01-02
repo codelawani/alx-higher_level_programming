0742ffe003f355f4d66a66576d98e4d00fde5180
Nico <nico>
2023-01-02 17:02:47 +0100

    add new files

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Creates a rectangle with height and width attributes"""
+
+    def __init__(self, width=0, height=0):
+        """Initialise Rectangle object"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """gets area of rectangle object"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """gets perimeter of rectangle object"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    @property
+    def width(self):
+        """Retrieves width of Rectangle object"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Sets width of Rectangle object"""
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        """Retrieves height of current object"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Sets height of current object"""
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
