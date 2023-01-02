2b18dcd436df6b14cfdd51282c9bf769585796e8
Nico <nico>
2023-01-02 16:23:08 +0100

    fix pycodestyle error

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 class Rectangle:
-	pass
+    pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
-	def __init__(self, width=0, height=0) -> None:
-		self.width = width
-		self.height = height
-	@property
-	def width(self):
-		return self._width
+    def __init__(self, width=0, height=0) -> None:
+        self.width = width
+        self.height = height
 
-	@width.setter
-	def width(self, width):
-		if not isinstance(width, int):
-			raise TypeError('width must be an integer')
-		elif width < 0:
-			raise ValueError('width must be >= 0')
-		self._width = width
-	@property
-	def height(self):
-		return self._height
+    @property
+    def width(self):
+        return self._width
 
-	@height.setter
-	def height(self, height):
-		if not isinstance(height, int):
-			raise TypeError('height must be an integer')
-		elif height < 0:
-			raise ValueError('height must be >= 0')
-		self._height = height
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self._height = height
