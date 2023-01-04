383fb2cf9692f3f766566b30338f13930b292e30
Nico <nico>
2023-01-04 21:27:16 +0100

    modify files

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Locked class"""
+
+
 class LockedClass:
+    """Locked class that only allows assignment of first name"""
+
     __slots__ = ['first_name']
 
     def __init__(self, value=None):
