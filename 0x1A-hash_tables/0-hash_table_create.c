0455edf6343157797257dc4194b61c10a50898ea
Nico <nico>
2023-01-05 11:16:42 +0100

    add new files launching my hash table  journey :)

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of table
+ *
+ * Return: pointer to table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return NULL;
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+	return table;
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+
+This directory contains solutions to practice problems I did on hash tables in C
