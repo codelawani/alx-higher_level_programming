aef1f33836ffebd3dc5c29d9b2e5b20d0d9e5727
Nico <lawani.gk@gmail.com>
2023-01-10 13:39:09 +0100

    add documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Define a write file function"""
+
+
 def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+    Args:
+        filename (any): file to be written to
+        text (any): text to be written to file
+    Returns:
+        The number of characters written"""
     with open(filename, 'w') as f:
         return f.write(text)
