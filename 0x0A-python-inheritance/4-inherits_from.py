2b9beb48e1f24140233bd9ef712a96067dc19ac2
Nico <lawani.gk@gmail.com>
2023-01-10 11:53:27 +0100

    add function that checks object inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that checks object inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class
+    Args:
+        obj (any): The object to check
+        a_class (type): The class to check object against
+    Returns:
+        if obj is inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
