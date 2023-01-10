3a496cf5bd7ca1b8ea4282255065310c79f457cb
Nico <lawani.gk@gmail.com>
2023-01-10 16:01:40 +0100

    add module that defines a student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a student class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
+
+
+print()
