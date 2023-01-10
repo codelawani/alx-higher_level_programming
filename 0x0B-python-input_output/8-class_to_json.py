41f3117c87079c535443b56b58b76c180eb54620
Nico <lawani.gk@gmail.com>
2023-01-10 15:49:03 +0100

    remove leading/trailing whitespaces

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Gets the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) 
+(list, dictionary, string, integer and boolean)
 for JSON serialization of an object"""
 
 
