ae6123ca4f8b91e5167e449014e07018b694ba1a
Nico <lawani.gk@gmail.com>
2023-01-10 12:59:47 +0100

    add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Defines a function that prints file content"""
+
+
 def read_file(filename=""):
+    """Prints read file contents to stdout
+    Args:
+        filename (str): file to be read"""
     with open(filename) as f:
         print(f.read().strip())
