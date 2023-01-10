1a0cea5a9803f61275ab3fdb1bbe2b5957e50d9f
Nico <lawani.gk@gmail.com>
2023-01-10 11:27:51 +0100

    add module that checks object instances

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module checks if an object is an instance
+of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ a function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False."""
+    return isinstance(obj, a_class)
