eb6d3d8d78c8e20b721f211879afe74b60a5eb90
Nico <lawani.gk@gmail.com>
2023-01-11 11:20:07 +0100

    modify file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Defines a rectangle class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
     def __init__(self, width, height):
-        super().integer_validator('width', width)
-        super().integer_validator('height', height)
+        """Assigns width and height attributes"""
+        self.integer_validator('width', width)
         self.__width = width
+        self.integer_validator('height', height)
         self.__height = height
+        super().integer_validator('height', height)
