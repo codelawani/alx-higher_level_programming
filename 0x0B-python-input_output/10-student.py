59501a0ca6defd0d4a9dd0c3df2b8fe30c9f05d0
Nico <lawani.gk@gmail.com>
2023-01-12 11:40:41 +0100

    modify file '10-student.py'

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,15 +13,28 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance"""
-        result = self.__dict__
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+
+""" class Student:
+"""     """Defines a student"""
+"""
+       def __init__(self, first_name, last_name, age):
+   """         """Initialises a student"""
+"""        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+"""         """retrieves a dictionary representation of a Student instance"""
+"""        result = self.__dict__
         if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             values = []
-            keys = []
             for key in attrs:
                 if key not in self.__dict__.keys():
                     continue
-                else:
-                    keys.append(key)
                 values.append(self.__dict__[key])
-            result = dict(zip(keys, values))
-        return result
+            result = dict(zip(attrs, values))
+        return result """
