d79350dd3247f2787c5628a687bc93e4c2a475f0
Nico <lawani.gk@gmail.com>
2023-01-16 22:13:37 +0100

    fix : models/rectangle.py::80: E501 line too long

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,4 +107,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle"""
-        return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x, 'y': self.y}
