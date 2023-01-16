fa258f983268c60b1c4dd020dbff8a5c248ea781
Nico <lawani.gk@gmail.com>
2023-01-16 22:46:11 +0100

    edit line 25

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
         if list_dictionaries or len(list_dictionaries):
             return json.dumps(list_dictionaries)
         else:
-            return "[]"
+            return '"[]"'
 
     @classmethod
     def save_to_file(cls, list_objs):
