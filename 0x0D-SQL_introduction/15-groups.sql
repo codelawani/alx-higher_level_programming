e7eee25fbf6cafbfb23466378c91f6b31caec441
angelofdeity <lawani.gk@gmail.com>
2023-02-15 03:45:52 -0800

    modify file '15-groups.sql'

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score
 --  in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score;
