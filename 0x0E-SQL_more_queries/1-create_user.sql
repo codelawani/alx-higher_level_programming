8c0d0e0e5956ae96ea0af039ff6a967b24d9a4b5
angelofdeity <lawani.gk@gmail.com>
2023-02-15 09:26:36 -0800

    modify files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- creates the MySQL server user user_0d_1
 -- user_0d_1 should have all privilege
 CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'%'
+	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
-	TO 'user_0d_1'@'%';
+	TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,9 @@
 CREATE DATABASE
 	IF NOT EXISTS hbtn_0d_2;
 CREATE USER
-	IF NOT EXISTS 'user_0d_2'@'%'
+	IF NOT EXISTS 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+	ON hbtn_0d_2.*
+	TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
