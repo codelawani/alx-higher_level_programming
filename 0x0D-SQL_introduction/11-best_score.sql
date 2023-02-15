91229bbf7d03d85266c6d2f42c20bc1c06105fcd
angelofdeity <lawani.gk@gmail.com>
2023-02-15 02:15:02 -0800

    add files '12-no_cheating.sql', '11-best_score.sql'

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+--  lists all records of the table second_table 
+-- of the database hbtn_0c_0
+SELECT score,name FROM second_table
+WHERE SCORE >= 10
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updates the score of Bob to 10
+-- in the table second_table
+UPDATE second_table SET SCORE = 10
+WHERE name = "Bob"
