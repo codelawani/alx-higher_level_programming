c4c3e09e3fcd98b7f6a439305e6240e5eb52a6a1
angelofdeity <lawani.gk@gmail.com>
2023-02-16 09:57:24 -0800

    fix 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,3 +5,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 	LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 	WHERE tv_show_genres.genre_id IS NULL
+	ORDER BY tv_shows.title, tv_show_genres.genre_id
