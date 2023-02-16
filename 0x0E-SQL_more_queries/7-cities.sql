f4b4dfd5c2478622fe848da5c7ad86c6ff187c68
angelofdeity <lawani.gk@gmail.com>
2023-02-16 01:49:10 -0800

    modify cities sql script

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,12 @@
 -- creates the database hbtn_0d_usa
--- creates the database hbtn_0d_usa
+-- and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE
 	IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
 CREATE TABLE
 	IF NOT EXISTS cities (
-		id        INT          NOT NULL UNIQUE PRIMARY KEY,
-		state_id INT          NOT NULL,
+		id        INT          NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+		state_id  INT          NOT NULL,
 		name      VARCHAR(256) NOT NULL,
-		FOREIGN KEY (state_id) REFERENCES states(state_id)
+		FOREIGN KEY (state_id) REFERENCES states(id)
 	);
