61de70019d9dd60fd3353504756f3cf4f814c2d7
Nico <lawani.gk@gmail.com>
2023-03-07 23:05:32 +0100

    modify 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const num = process.argv.slice(2).sort().reverse()[1];
+let num = process.argv.slice(2).sort((a, b) => b - a)[1];
 // second largest is the number at index 1 in the array
 if (num) {
   console.log(num);
