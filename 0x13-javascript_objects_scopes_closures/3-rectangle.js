aef022b4554b84f9483dc4e1ca51d450c8edbd2a
Nico <lawani.gk@gmail.com>
2023-03-09 19:13:48 +0100

    add new rectangle script

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    while (this.height-- > 0) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
