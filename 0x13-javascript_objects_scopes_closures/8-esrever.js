65b1048eade0f7f0923f186122277b75d06ba429
Nico <lawani.gk@gmail.com>
2023-03-10 18:40:10 +0100

    refactor code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let end = list.length - 1;
-  const mid = parseInt(list.length / 2);
+  /* let end = list.length - 1;
+  const mid = list.length >> 1; // calculate midpoint using bitwise right shift
+  // loop through first half of array
   for (let start = 0; start < mid; start++) {
     const temp = list[start];
     list[start] = list[end];
     list[end] = temp;
     end--;
   }
-  return list;
+  return list; */
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
 };
