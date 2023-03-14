0b4787d27afcb985b233eb24a859fcdc07604dc1
Nico <lawani.gk@gmail.com>
2023-03-14 11:38:16 +0100

    add file 102-concat.j

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const fs = require('fs');
+const [srcfile1, srcfile2, destfile] = process.argv.slice(2);
+const filecontent1 = fs.readFileSync(srcfile1, 'utf-8');
+const filecontent2 = fs.readFileSync(srcfile2, 'utf-8');
+const newContent = filecontent1 + filecontent2;
+fs.writeFileSync(destfile, newContent);
