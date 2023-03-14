c5d8d17637f4e801e7e08d4c2afe241dbcff0be5
Nico <lawani.gk@gmail.com>
2023-03-14 11:20:31 +0100

    modify 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,32 @@
 #!/usr/bin/node
-const dict = require('./101-data.js');
-for (let key in dict) {
-  idlist = []
-  if (dict[key])
+const oldDict = require('./101-data.js').dict;
+/* exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+}; */
+const newDict = {};
+for (const key in oldDict) {
+  const value = oldDict[key];
+  // check if the value in old dict is in the newDict
+  /* if its there, push its key to the newDict
+    example:
+      oldDict
+      key: value
+      { 89: 1 }
+      { 91: 1 }
+      newDict
+      key: value
+      { 1: [89] } // on first iteration
+      { 1: [89, 91]} // on second iteration
+  */
+  if (value in newDict) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
+  }
 }
+console.log(newDict);
