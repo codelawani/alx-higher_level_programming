727aada29d12999a61f23b911b7f4d114ccecc34
angelofdeity <lawani.gk@gmail.com>
2023-03-18 06:41:50 -0700

    modify 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from sys import argv as av
-import MySQLdb
-
 """
 script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa
@@ -10,13 +7,17 @@ This script should take 3 arguments:
 mysql username, mysql password,
 database name and state name searched
 """
-st_name = av[4]
-db = MySQLdb.connect("localhost", *av[1:4], 3306)
-cursor = db.cursor()
-query = "SELECT * FROM states WHERE name LIKE '%s' ORDER BY states.id" % (st_name,)
-cursor.execute(query)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-cursor.close()
-db.close()
+
+from sys import argv as av
+import MySQLdb
+if __name__ == '__main__':
+    st_name = av[4]
+    db = MySQLdb.connect("localhost", *av[1:4], 3306)
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE '%s' ORDER BY states.id" % (st_name,)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
