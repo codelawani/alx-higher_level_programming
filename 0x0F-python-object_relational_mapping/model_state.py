03fdb1b1c2252688ab13324225b98dca4a3c0a40
Nico <lawani.gk@gmail.com>
2023-03-18 23:27:55 +0100

    refactor code

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,13 +9,16 @@ from sqlalchemy import Column, Integer, String
 
 from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    Base = declarative_base()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base = declarative_base()
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    class State(Base):
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
-    Base.metadata.create_all(engine)
+
+class State(Base):
+    """creates a state object"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
