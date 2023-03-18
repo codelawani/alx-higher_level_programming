1c6f26fa307e9727450f5f805d3ec389c19ceeef
angelofdeity <lawani.gk@gmail.com>
2023-03-18 06:00:25 -0700

    refactor code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,21 +9,12 @@ This script should take 3 arguments:
 mysql username, mysql password and database name
 """
 
+db = MySQLdb.connect("localhost", *av[1:], 3306)
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states ORDER BY states.id")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+db.close()
 
-def main():
-    """
-    Driver code
-    """
-
-    db = MySQLdb.connect("localhost", *av[1:], 3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
-
-
-if __name__ == '__main__':
-    main()
