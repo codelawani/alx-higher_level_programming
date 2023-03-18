331e19e1b6d09a8bb72e9c2a1c7355816279b728
angelofdeity <lawani.gk@gmail.com>
2023-03-18 07:09:03 -0700

    modify 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,12 +10,14 @@ This script should take 3 arguments:
 mysql username, mysql password,
 database name and state name searched
 """
-st_name = av[4]
-db = MySQLdb.connect("localhost", *av[1:4], 3306)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id",(st_name,))
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-cursor.close()
-db.close()
+if __name__ == '__main__':
+    st_name = av[4]
+    db = MySQLdb.connect("localhost", *av[1:4], 3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE \
+name LIKE BINARY %s",(st_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
