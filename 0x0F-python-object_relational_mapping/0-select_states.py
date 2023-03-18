7f66ac937ff35882e9a780da23baf401cfbbf3a0
angelofdeity <lawani.gk@gmail.com>
2023-03-18 06:16:46 -0700

    refactor code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""
+a script that lists all states from
+the database hbtn_0e_0_usa
+This script should take 3 arguments:
+mysql username, mysql password and database name
+"""
 
+from sys import argv as av
+import MySQLdb
 
-def main():
-    from sys import argv as av
-    import MySQLdb
-
-    """
-    a script that lists all states from
-    the database hbtn_0e_0_usa
-    This script should take 3 arguments:
-    mysql username, mysql password and database name
-    """
-
+if __name__ == '__main__':
     db = MySQLdb.connect("localhost", *av[1:], 3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
@@ -21,6 +19,3 @@ def main():
     cursor.close()
     db.close()
 
-
-if __name__ == '__main__':
-    main()
