f3b31bd205de03088f8547039d7af7cce9f05b60
angelofdeity <lawani.gk@gmail.com>
2023-03-18 08:10:07 -0700

    refactor code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     """Driver code"""
     db = MySQLdb.connect("localhost", *av[1:4], 3306)
     cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities \
-JOIN states ON cities.state_id = states.id\
-WHERE state.name = '%s' ORDER BY cities.id", (av[4],))
-    print(cursor.fetchall())
+    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id \
+WHERE states.name = %s ORDER BY cities.id", (av[4],))
+    cities = [row[0] for row in cursor.fetchall()]
+    print(", ".join(cities))
     cursor.close()
     db.close()
