3dcd11e61475730638a78fce62a08a74c17cc86e
Nico <lawani.gk@gmail.com>
2023-03-19 21:14:09 +0100

    9-model_state_filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """This script"""
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for i in states:
+        print(f'{i.id}: {i.name}')
+    session.close()
