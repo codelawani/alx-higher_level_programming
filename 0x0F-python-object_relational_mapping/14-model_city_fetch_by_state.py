b5cc9ef04bd9c27b2bdcc8bf257a84e7334abd8d
Nico <lawani.gk@gmail.com>
2023-03-20 01:20:01 +0100

    add file 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script lists all City objects
+from the database hbtn_0e_14_usa"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id)[0:15]:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script contains the class definition of a City"""
+
+from model_state import State, Base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Creates a city object"""
+    __tablename__ = 'cities'
+
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey(
+        'states.id'), nullable=False)
