5890556b78b9d6e3a1868475ed68d453920f6c5a
Nico <lawani.gk@gmail.com>
2023-03-30 18:27:25 +0100

    begin 0x10-python-network_0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script takes in a URL, sends a GET request to the URL,
+# and displays the body of the response
+curl -sL "$1"
