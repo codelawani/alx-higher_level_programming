962d23bd3e32a71ba34bd47c8145ef94e511313d
Nico <lawani.gk@gmail.com>
2023-03-30 18:28:39 +0100

    modify files

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# This script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
