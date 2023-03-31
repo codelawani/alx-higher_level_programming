aecc7d0712cf1f5ea3b4be7635f16e90d91455fe
Nico <lawani.gk@gmail.com>
2023-03-31 15:42:16 +0100

    update new files

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """This script"""
+
+from sys import argv
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
--- 0x11-python-network_1/duerror.py
+++ 0x11-python-network_1/duerror.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """This script"""
+
+from urllib import request, error
+from sys import argv
+
+try:
+    r = request.urlopen(argv[1])
+except error.HTTPError as e:
+    print(f'Error code: {e.code}')
+else:
+    print(r.read())
