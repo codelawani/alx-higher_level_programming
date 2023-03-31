3e78afc10682a5e504a3de463d58976f9c3bf33a
Nico <lawani.gk@gmail.com>
2023-03-31 15:25:55 +0100

    update new files

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script takes your Github credentials (username and password) and
+    uses the Github API to display your id"""
+from sys import argv
+import requests
+if __name__ == "__main__":
+    response = requests.get(
+        'https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(response.json().get('id'))
