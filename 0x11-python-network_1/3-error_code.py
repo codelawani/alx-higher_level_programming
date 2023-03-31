6f262fa26b7e477e64171af5a4f73975c882ed42
Nico <lawani.gk@gmail.com>
2023-03-31 15:43:22 +0100

    update doctstring of new file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script takes in a URL, sends a request to the URL and displays the
+    body of the response (decoded in utf-8)"""
 
 from sys import argv
 from urllib import request, error
