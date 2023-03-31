8e5a5d5de8cce9a8f291d98583b7501a80ca8fbb
Nico <lawani.gk@gmail.com>
2023-03-31 16:31:10 +0100

    update new file with try block to handle exceptions

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script prints the 10 most recent commits on the
+    master branch of the repository “rails” by the user "rails"""
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    REPO = argv[1]
+    OWNER = argv[2]
+    url = f'https://api.github.com/repos/{OWNER}/{REPO}/commits'
+    resp = get(url)
+
+    for i in range(10):
+        try:
+            NAME = resp.json()[i].get('commit').get('author').get('name')
+            SHA = resp.json()[i].get('sha')
+            print(f'{SHA}: {NAME}')
+        except (AttributeError, IndexError):
+            break
