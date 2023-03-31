293c51c76de7d81e02964b6b7514b2ffa1231e82
Nico <lawani.gk@gmail.com>
2023-03-31 12:44:29 +0100

    modify file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This script fetches a header from https://intranet.hbtn.io/status"""
 
+from urllib import request
+from sys import argv
 if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen(argv[1]) as response:
         print(response.getheader('X-Request-Id'))
