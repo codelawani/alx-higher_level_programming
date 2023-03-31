aa6d971694c382bfe130a9d194e3b07d9298dbf6
Nico <lawani.gk@gmail.com>
2023-03-31 12:52:16 +0100

    modify file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This script fetches https://intranet.hbtn.io/status"""
+"""This script fetches https://intranet.hbtn.io/status
+    using the urllib package"""
 
-import urllib.request as request
 if __name__ == "__main__":
+    import urllib.request as request
+
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
