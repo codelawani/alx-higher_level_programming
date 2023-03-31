27957b590e7898039598e2edc3fa33ffdb5e9164
Nico <lawani.gk@gmail.com>
2023-03-31 11:48:48 +0100

    initialise 0x11-python-network_1

--- 0x10-python-network_0/0x11-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_0/0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script fetches https://intranet.hbtn.io/status"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
--- 0x10-python-network_0/0x11-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_0/0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script """
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        print(response.getheader('X-Request-Id'))
