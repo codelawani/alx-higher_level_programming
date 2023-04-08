5c656cc1133fff33783e54394db1b3cb55d07150
Nico <lawani.gk@gmail.com>
2023-04-08 19:08:46 +0100

    modify file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script finds a peak in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
     """finds peak"""
     listints = list_of_integers
@@ -10,14 +11,19 @@ def find_peak(list_of_integers):
         return None
     for i in range(low, high):
         mid = low + (high - low) // 2
-        if (listints[mid] > listints[mid - 1]) and (listints[mid] > listints[mid + 1]):
+        if (listints[mid] > listints[mid - 1]) and
+        (listints[mid] > listints[mid + 1]):
             return listints[mid]
-        elif listints[low] > listints[low + 1] and not listints[:low]:
+        elif listints[low] > listints[low + 1] and
+        not listints[:low]:
             return listints[low]
-        if listints[high] > listints[high - 1] and not listints[high + 1:]:
+        if listints[high] > listints[high - 1] and
+        not listints[high + 1:]:
             return listints[high]
         elif listints[mid] > listints[mid + 1]:
             low = mid
+        elif listints[mid] > listints[mid - 1]:
+            high = mid
         else:
             high = mid
     return listints[i]
