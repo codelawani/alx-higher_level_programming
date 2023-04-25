900b99eaa9834c5b42197602a910ade0969c0191
Nico <lawani.gk@gmail.com>
2023-04-25 16:10:18 +0100

    update new script

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+request.get(process.argv[2], (err, response, body) => {
+  if (!err) {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
+  }
+});
