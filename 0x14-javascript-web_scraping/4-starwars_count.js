f83ff4b0954d34e1432c80acb4c45e4a1ec9d8c4
Nico <lawani.gk@gmail.com>
2023-04-25 15:47:56 +0100

    refactor code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
-const characterApi = 'https://swapi-api.alx-tools.com/api/people/';
-const characterId = 18;
+const characterApi = 'https://swapi-api.alx-tools.com/api/people/18/';
+const movieApi = process.argv[2];
 const rq = require('request');
-rq.get(
-  { url: characterApi + characterId, encoding: 'utf-8' },
-  (err, response, body) => {
-    if (!err) {
-      //  Number of films specified character appears in
-      console.log(JSON.parse(body).films.length);
+rq.get({ url: movieApi, encoding: 'utf-8' }, (err, response, body) => {
+  if (!err) {
+    //  Number of films specified character appears in
+    let count = 0;
+    for (const result of JSON.parse(body).results) {
+      for (const c of result.characters) {
+        if (c === characterApi) {
+          count++;
+        }
+      }
     }
+    console.log(count);
   }
-);
+});
