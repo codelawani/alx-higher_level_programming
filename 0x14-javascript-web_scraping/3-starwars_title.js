b293cfd1534556093669c868cff7e1661dbc2354
Nico <lawani.gk@gmail.com>
2023-04-25 15:28:58 +0100

    modify js scripts

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const api = 'https://swapi-api.alx-tools.com/api/films/';
-const id = process.argv[2];
+const movieApi = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = process.argv[2];
 const rq = require('request');
-rq.get({ url: api + id, encoding: 'utf-8' }, (err, response, body) => {
-  if (!err) {
-    console.log(JSON.parse(body).title);
+rq.get(
+  { url: movieApi + movieId, encoding: 'utf-8' },
+  (err, response, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).title);
+    }
   }
-});
+);
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const characterApi = 'https://swapi-api.alx-tools.com/api/people/';
+const characterId = 18;
+const rq = require('request');
+rq.get(
+  { url: characterApi + characterId, encoding: 'utf-8' },
+  (err, response, body) => {
+    if (!err) {
+      //  Number of films specified character appears in
+      console.log(JSON.parse(body).films.length);
+    }
+  }
+);
