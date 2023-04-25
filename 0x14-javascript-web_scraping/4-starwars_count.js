54cd25e5cee63830833d527be96637ce8bdef062
Nico <lawani.gk@gmail.com>
2023-04-25 16:07:01 +0100

    refactor code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,29 @@
 #!/usr/bin/node
-const characterApi = 'https://swapi-api.alx-tools.com/api/people/18/';
-const movieApi = process.argv[2];
-const rq = require('request');
-rq.get({ url: movieApi, encoding: 'utf-8' }, (err, response, body) => {
-  if (!err) {
-    //  Number of films specified character appears in
-    let count = 0;
-    for (const result of JSON.parse(body).results) {
-      for (const c of result.characters) {
-        if (c === characterApi) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+const request = require('request');
+
+// Get the API URL from the command-line arguments
+const apiUrl = process.argv[2];
+
+// ID of the character "Wedge Antilles"
+const characterId = 18;
+const characterApi = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
+// Make a request to the movies API endpoint
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    return;
   }
+  if (response.statusCode !== 200) {
+    return;
+  }
+
+  // Parse the response body as JSON
+  const movieData = JSON.parse(body);
+
+  // Filter the movies based on the presence of the character "Wedge Antilles"
+  const wedgeMovies = movieData.results.reduce((count, movie) => {
+    return movie.characters.includes(characterApi) ? count + 1 : count;
+  }, 0);
+
+  // Print the total number of movies where "Wedge Antilles" appears
+  console.log(wedgeMovies);
 });
