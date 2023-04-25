f0d776d0529421ab607c11108d71dcf6b3f25663
Nico <lawani.gk@gmail.com>
2023-04-25 16:13:27 +0100

    update new script

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      if (tasks[i].completed === true) {
+        if (completed[tasks[i].userId]) {
+          completed[tasks[i].userId] += 1;
+        } else {
+          completed[tasks[i].userId] = 1;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
