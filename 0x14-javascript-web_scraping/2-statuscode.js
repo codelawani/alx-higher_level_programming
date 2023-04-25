6be4d77080d6b9b151f9bfc62397bf2547dc29f5
Nico <lawani.gk@gmail.com>
2023-04-25 14:48:23 +0100

    update 2-statuscode.js script

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], (err, response, body) => {
+  if (!err) {
+    console.log('code:', response.statusCode);
+  }
+});
