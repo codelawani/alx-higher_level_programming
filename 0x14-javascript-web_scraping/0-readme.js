1058ec24c5bdf9207066cda0bc21964a83a931d1
Nico <lawani.gk@gmail.com>
2023-04-25 13:39:16 +0100

    add readme and update new js script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  if (data) {
+    console.log(data);
+  }
+});
