4426bf2419211b43749f462bcd28fda9b63a7f60
Nico <lawani.gk@gmail.com>
2023-04-25 16:30:47 +0100

    update new script

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,39 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2]; // Get the movie ID from the command-line argument
+
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(movieUrl, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`HTTP status code: ${response.statusCode}`);
+  } else {
+    const movieData = JSON.parse(body);
+    const charactersUrls = movieData.characters;
+    const charactersData = [];
+    let charactersCount = 0;
+
+    charactersUrls.forEach((characterUrl, index) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+        } else if (response.statusCode !== 200) {
+          console.error(`HTTP status code: ${response.statusCode}`);
+        } else {
+          const characterData = JSON.parse(body);
+          charactersData[index] = characterData;
+          charactersCount++;
+          if (charactersCount === charactersUrls.length) {
+            // All characters have been fetched, print their names
+            charactersData.forEach((character) => {
+              console.log(character.name);
+            });
+          }
+        }
+      });
+    });
+  }
+});
