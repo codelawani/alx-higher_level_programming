7b75fdd392401b33e15bfdd2d4b90b384122627f
Nico <lawani.gk@gmail.com>
2023-04-25 15:07:29 +0100

    update new js script

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const api = 'https://swapi-api.alx-tools.com/api/films/';
+const id = process.argv[2];
+const rq = require('request');
+rq.get({ url: api + id, encoding: 'utf-8' }, (err, response, body) => {
+  if (!err) {
+    console.log(JSON.parse(body).title);
+  }
+});
